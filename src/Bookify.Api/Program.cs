1a11db46dd7a908dbca8ec328067cd03b96f8e8d
DESKTOP-FTVMTJF\user <matthewhoung@gmail.com>
2024-08-08 11:59:34 +0800

    added the initial migration to the pgsql container.

--- src/Bookify.Api/Program.cs
+++ src/Bookify.Api/Program.cs
@@ -22,7 +22,7 @@ if (app.Environment.IsDevelopment())
     app.ApplyMigrations();
 
     //REMARK: Uncomment if you want to seed initial data.
-    app.SeedData();
+    //app.SeedData();
 }
 
 app.UseHttpsRedirection();
